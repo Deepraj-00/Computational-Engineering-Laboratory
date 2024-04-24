3c3603da72a36c462006631adba4fc2da83b6155
Deepraj-00 <morideeprajsinh999@gmail.com>
2024-04-25 02:24:15 +0530

    Create Lab_2_Numerical2.m

---
+++ Lab_2_Numerical2.m
@@ -0,0 +1,10 @@
+% Initialize the sum
+totalSum = 0;
+
+% Use a FOR loop to iterate from 1 to 100
+for i = 1:100
+    totalSum = totalSum+i;
+end
+
+% Display the result
+fprintf('The sum of numbers from 1 to 100 is: %d\n',totalSum);
